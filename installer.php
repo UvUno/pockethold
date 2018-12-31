8655403436efae5528b087d5a974a4724657ded8
Andre Herberth <andre.thoresen@gmail.com>
2018-12-31 13:13:44 +0100

    Moved and replaced installer.php

--- installer.php
+++ installer.php
@@ -1,57 +1,56 @@
-<?php
-/*
-This File is part of the Pockethold 3rdparty flarum installer.
-*/
-
-// Run Installer
-if(isset($_REQUEST['ajax']) && !empty($_REQUEST["ajax"])) {
-    require_once("pockethold/loader.php");
-    if ( !defined('ABSPATH') )
-    {
-        define('ABSPATH', dirname(__FILE__) . '/');
-    }
-    $tmppath = (ABSPATH . 'pockethold/');
-
-    // Listen for Ajax Calls
-    $ear = new Pockethold(ABSPATH, $tmppath);
-    echo $ear->listen($_REQUEST['ajax']);
-}
-else {
-    ?>
-
-    <!DOCTYPE html>
-    <html lang="en">
+<!doctype html>
+<html>
     <head>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-		<title>Pockethold: 3rdparty Flarum Installer</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Pockethold: Flarum Downloader</title>
+		<link rel="stylesheet" type="text/css" href="./pockethold/assets/style.css">
     </head>
     <body>
-    <div class="container">
-        <div class="jumbotron" style="background-color: transparent;">
-            <div class="container text-center">
-
-                <img style="margin: auto;" class="img-responsive" alt="Pockethold"
-                     src="pockethold/assets/logo.png"/>
-                <p style="max-width: 460px; margin:auto;">Pockethold is a 3rd party no shell Flarum downloader.</p>
-
-                <div id="progressdiv" style="margin-top:50px;">
-                    <p style="max-width: 460px; margin:50px auto auto auto;">
-                        <button id="checkingbtn" class="instal1 btn btn-default btn-lg" role="button" disabled>Getting Status<i class="fa fa-cog fa-spin"></i></button>
-                    </p>
-                </div>
-
-            </div>
-        </div>
-    </div>
-    <script type="text/javascript" src="pockethold/assets/pockethold.js"></script>
-    </body>
-    </html>
-
-    <?php
-}
-
-
-?>
+		<header class="header">
+			<div class="container">
+				<div class="logo_container">
+				<span class="logo_helper"></span>				
+				<img src="./pockethold/assets/logo.png" alt="Pockethold" id="logo">
+				</div>
+			</div>
+		</header>
+		
+		<div id="pockethold" class="container pockethold">
+			
+		</div>
+		
+		<footer class="footer">
+			<div class="container">
+				<div class="footer-widgets">
+					<div class="footer-widget">
+						<h4>Disclaimer</h4>
+						<em>Pockethold is not affiliated, funded or associated with the Flarum Project. Keep this in mind.</em>
+					</div>
+					<div class="footer-widget"><h4>Contribute</h4>
+						<ul>
+						  <li><a href="/#">Github</a></li>
+						  <li><a href="https://discuss.flarum.org/d/5731-pockethold-a-web-based-installer">Forum Thread</a></li>
+						  <li><a href="https://pockethold.tk/contribute">Website</a></li>
+						</ul> 
+					</div>
+					<div class="footer-widget"><h4>Get Help</h4>
+						<ul>
+						  <li><a href="https://pockethold.tk/my-tickets/">Create Ticket</a></li>
+						  <li><a href="/#">Discord</a></li>
+						  <li><a href="https://discuss.flarum.org/d/5731-pockethold-a-web-based-installer">Forum Thread</a></li>
+						</ul> 
+					</div>
+					<div class="footer-widget zero-right"><h4>Resources</h4>
+						<ul>
+						  <li><a href="https://pockethold.tk">Pockethold Website</a></li>
+						  <li>-----------</li>
+						  <li><a href="https://flagrow.io">Flagrow.io</a></li>
+						</ul> 
+					</div>
+					</div>
+			</div>
+		</footer>
+		<script src="./pockethold/assets/pockethold.js"></script>
+	</body>
+</html>
